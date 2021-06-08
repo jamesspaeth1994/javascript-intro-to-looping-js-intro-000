dd1a26b6f7aa685f570129bdcd87b3237f81506c
reflective-foobar-0518 <jamesspaeth1994@gmail.com>
2021-06-08 23:03:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,11 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i === 1 && i === 0) {
-      return "I am 1 strange loop."
-    } else {
-      "I am ${i} strange loop${i === 0 ? '' : 's'}."
+    if (i === 1 ){
+      array[i] = "I am 1 strange loop."
+    }
+    else{
+      array[i] = `I am ${i} strange loops.`
     }
   }
-}
+  console.log(array);
+  return array;
